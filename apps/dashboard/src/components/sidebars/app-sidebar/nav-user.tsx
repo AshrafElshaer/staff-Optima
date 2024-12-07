5309319f78b90efc91bd3ded34c8f15d9b86d21f
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-12-06 23:10:51 -0600

    adjust sidebars items

--- apps/dashboard/src/components/sidebars/app-sidebar/nav-user.tsx
+++ apps/dashboard/src/components/sidebars/app-sidebar/nav-user.tsx
@@ -63,7 +63,7 @@ export function NavUser({
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className={cn(" rounded-md w-[11rem]")}
+            className={cn(" rounded-md w-[12rem]")}
             side="top"
             align="center"
             sideOffset={4}
