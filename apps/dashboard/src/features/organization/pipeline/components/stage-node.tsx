a8d3bc085b3a6f520641688a11104f4cd39b9ec4
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2024-12-30 11:33:56 -0600

    clean up imports

--- apps/dashboard/src/features/organization/pipeline/components/stage-node.tsx
+++ apps/dashboard/src/features/organization/pipeline/components/stage-node.tsx
@@ -1,30 +1,11 @@
 import type { ApplicationStage } from "@optima/supabase/types";
 import type { pipelineStageSchema } from "@optima/supabase/validations";
-import { Button } from "@optima/ui/button";
+
 import { cn } from "@optima/ui/cn";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@optima/ui/dropdown-menu";
-import {
-  Handle,
-  type Node,
-  type NodeProps,
-  NodeToolbar,
-  Position,
-  useReactFlow,
-} from "@xyflow/react";
-import { Delete01Icon } from "hugeicons-react";
-import { ArrowUpDownIcon, Sparkle, Sparkles, Trash } from "lucide-react";
-import { useAction } from "next-safe-action/hooks";
-import { toast } from "sonner";
+
+import { Handle, type Node, type NodeProps, Position } from "@xyflow/react";
+
 import type { z } from "zod";
-import {
-  deleteApplicationStageAction,
-  reorderApplicationStagesAction,
-} from "../stages-pipeline.actions";
 import { DeleteStage } from "./delete-stage";
 import { ReorderStages } from "./reorder-stages";
 
@@ -33,38 +14,6 @@ export type StageNode = Node<z.infer<typeof pipelineStageSchema>, "stageNode">;
 export function StageNode(props: NodeProps<StageNode>) {
   const { data, selected } = props;
   const { title, description, indicator_color, stage_order, id } = data;
-  const { setNodes, setEdges, getNodes } = useReactFlow();
-
-  const { executeAsync: deleteApplicationStage, isExecuting: isDeleting } =
-    useAction(deleteApplicationStageAction, {
-      onSuccess: ({ data }) => {
-        setNodes((nodes) =>
-          nodes
-            .filter((node) => node.id !== id)
-            .map((node, idx) => ({
-              ...node,
-              position: { x: 0, y: idx * 250 },
-            })),
-        );
-        if (data) {
-          setEdges(
-            data.map((stage, idx) => ({
-              id: `e${stage.id}-${data[idx + 1]?.id}`,
-              source: stage.id,
-              target: data[idx + 1]?.id ?? "rejected",
-              // animated: true,
-              // type: "stageEdge",
-            })),
-          );
-        }
-      },
-    });
-
-
-
-
-
-
 
   return (
     <>
