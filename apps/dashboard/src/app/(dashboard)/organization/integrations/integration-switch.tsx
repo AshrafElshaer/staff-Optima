aac9a30165006cde1ba85efb464f558b50b21755
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-01-09 14:51:02 -0600

    add integration switch ui

---
+++ apps/dashboard/src/app/(dashboard)/organization/integrations/integration-switch.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Label } from "@optima/ui/label";
+import { Switch } from "@optima/ui/switch";
+import { useId, useState,  } from "react";
+
+type IntegrationSwitchProps = {
+  name: string;
+  description: string;
+  status: "connected" | "disconnected";
+  icon: React.ReactNode;
+};
+
+export default function IntegrationSwitch({
+  name,
+  description,
+  status,
+  icon,
+}: IntegrationSwitchProps) {
+  const id = useId();
+  const [isChecked, setIsChecked] = useState(status === "connected");
+  return (
+    <div className="relative flex w-full items-start gap-2 rounded-lg border  p-4 shadow-sm shadow-black/5 has-[[data-state=checked]]:border-ring/50">
+      <Switch
+        id={id}
+        className="order-1 h-4 w-6 after:absolute after:inset-0 [&_span]:size-3 [&_span]:data-[state=checked]:translate-x-2 rtl:[&_span]:data-[state=checked]:-translate-x-2"
+        aria-describedby={`${id}-description`}
+        checked={isChecked}
+        onCheckedChange={setIsChecked}
+      />
+      <div className="flex grow items-start gap-3">
+        {icon}
+        <div className="grid grow gap-2">
+          <Label htmlFor={id} className="text-lg font-semibold">
+            {name}
+            {/* <span className="text-xs font-normal leading-[inherit] text-muted-foreground">
+              (Sublabel)
+            </span> */}
+          </Label>
+          <p
+            id={`${id}-description`}
+            className="text text-secondary-foreground"
+          >
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- apps/dashboard/src/app/(dashboard)/organization/integrations/page.tsx
+++ apps/dashboard/src/app/(dashboard)/organization/integrations/page.tsx
@@ -1,7 +1,49 @@
+import { PageTitle } from "@/components/page-title";
+import { FaCalendar, FaLinkedinIn, FaSlack, FaVideo } from "react-icons/fa";
+import IntegrationSwitch from "./integration-switch";
+
+const integrations: {
+  name: string;
+  icon: React.ReactNode;
+  description: string;
+  status: "connected" | "disconnected";
+}[] = [
+  {
+    name: "LinkedIn",
+    icon: <FaLinkedinIn size={32} className="text-blue" />,
+    description:
+      "Connect your LinkedIn account to sync your candidates and jobs.",
+    status: "connected",
+  },
+  {
+    name: "Slack",
+    icon: <FaSlack size={32} />,
+    description: "Connect your Slack account to sync your candidates and jobs.",
+    status: "disconnected",
+  },
+  {
+    name: "Google Calendar",
+    icon: <FaCalendar size={28} />,
+    description: "Connect your Google Calendar to sync your events.",
+    status: "disconnected",
+  },
+  {
+    name: "Google Meet",
+    icon: <FaVideo size={28} />,
+    description: "Connect your Google Meet to sync your events.",
+    status: "disconnected",
+  },
+];
+
 export default function OrganizationIntegrationsPage() {
   return (
-    <div>
-      <h1>OrganizationIntegrations</h1>
+    <div className="flex flex-col gap-6 flex-1">
+      <PageTitle title="Connect and Manage Integration." className="!w-full" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {integrations.map((integration) => (
+          <IntegrationSwitch key={integration.name} {...integration} />
+        ))}
+      </div>
     </div>
   );
 }
